123df2eddc71f8b407415d0c52fbaea8803d7ee8
Macaulay-Reiss Christian <macaulay-reisschristian@macaulaeisssmbp.home>
2018-08-10 21:26:04 +0100

    Small chnages in the css and added external js script for future expansion

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function OpenSlideMenu() {
+    document.getElementById('side-menu').style.width = '200px';
+    document.getElementById("menu").style.display = 'none';
+}
+
+function CloseSlideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById("menu").style.display = 'block';
+}
