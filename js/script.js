740747c9057d74095ee00af6ef21038d82d8a824
Macaulay-Reiss Christian <macaulay-reisschristian@unknown.home>
2018-08-11 18:32:13 +0100

    Removed navigation and Fixed a bug with padding

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function OpenSlideMenu() {
-    document.getElementById('side-menu').style.width = '200px';
-    document.getElementById("menu").style.display = 'none';
-}
-
-function CloseSlideMenu() {
-    document.getElementById('side-menu').style.width = '0';
-    document.getElementById("menu").style.display = 'block';
-}
