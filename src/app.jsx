9d8baa1ba52a0862d1291c16a529dc47c3f83b1b
Macaulay-Reiss Christian <christianm@cardiff.ac.uk>
2019-05-23 18:54:18 +0100

    sass setup

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,10 @@
 import React, { Component, Fragment } from 'react'
-import styles from './styles/main.scss'
 
 const content = 'COMING SOON!'
 
 const App = () => (
   <Fragment>
-    <div className={styles['resizeable-both']}>{content}</div>
+    <div>{content}</div>
   </Fragment>
 )
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 
+import './scss/main.scss'
+
 ReactDOM.render(<App />, document.getElementById('root'))
 
 if (module.hot) {
